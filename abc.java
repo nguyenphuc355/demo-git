f5eefc7f57da6566e111737d06d729131213803d
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2024-04-14 23:07:08 +0700

    update abc

--- abc.java
+++ abc.java
@@ -1,1 +1,9 @@
-System.out.println("hello word")
+public class HelloWorld {
+	public static void main(String[] fuck) {
+		System.out.print("fuck you");
+		System.out.print("fuck you");
+	}
+	public static integer tichAB(int a, int b){
+		return a*b;
+	}
+}
