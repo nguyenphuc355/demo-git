f19807882aa9cfd888525be52faeeb0540e4f69a
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2024-04-21 21:59:06 +0700

    lấy 1 file từ branch khác

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] fuck) {
+		System.out.print("fuck you");
+		System.out.print("fuck you");
+	}
+	public static <integer> int tichAB(int a, int b){
+		return a*b;
+	}
+}
